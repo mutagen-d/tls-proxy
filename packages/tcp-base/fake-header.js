b463aecd10dc59513699e200b1d52bf126c1df79
MutagenD <mutagen-d@protonmail.com>
2026-04-09 13:28:27 +0300

    fix(tcp-base): add missed method

--- packages/tcp-base/fake-header.js
+++ packages/tcp-base/fake-header.js
@@ -72,6 +72,27 @@ const randomMethod = () => {
   const method = methods[index]
   return method
 }
+const randomUrl = () => {
+  const size = Math.floor(Math.random() * 10) + 5;
+  const id = crypto.randomBytes(size).toString('hex')
+  /** @type {['images', 'videos', 'users']} */
+  const names = [
+    'images',
+    'videos',
+    'users',
+    'files',
+  ]
+  const index = crypto.randomInt(names.length)
+  const name = names[index]
+  if (crypto.randomInt(10) < 5) {
+    /** @type {`/api/${name}/${id}`} */
+    const url = `/api/${name}/${id}`
+    return url
+  }
+  /** @type {`/api/${name}`} */
+  const url = `/api/${name}`
+  return url
+}
 /**
  * @param {string | {
  *  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'HEAD'
